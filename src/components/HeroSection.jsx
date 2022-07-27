be1461985364daa42bf05755a503b8319c4d87cd
vivekmodi1301 <modivivek03@gmail.com>
2022-07-28 00:13:16 +0530

    Hero Section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import { Button } from '../styles/Button';
+
+const HeroSection = () => {
+    return (
+        <Wrapper>
+            <div className="container grid grid-two-column">
+                <div className='section-hero-data'>
+                    <p className='hero-top-data'>This is me</p>
+                    <h1 className='hero-heading'>VIVEK MODI</h1>
+                    <p className='hero-para'>
+                        I'm Vivek Girish Modi . Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                        Perferendis ducimus consectetur ab fugit consequuntur. Architecto culpa, illo labore eius.
+                    </p>
+                    <Button className="btn hireme-btn">
+                        <NavLink to="/contact">Hire Me</NavLink>
+                    </Button>
+                </div>
+                <div className='section-hero-image'>
+                    <picture>
+                        <img src="./images/hero_banner.svg" alt="Hero Banner Image" className='hero-img' />
+                    </picture>
+                </div>
+            </div>
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.section`
+    padding: 9rem 0;
+
+    .section-hero-data {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .btn{
+        max-width: 16rem;
+    }
+
+    .hero-top-data{
+        text-transform: uppercase;
+        font-weight: 500;
+        font-size: 1.5rem;
+        color: ${({ theme }) => theme.colors.helper};
+    }
+
+    .hero-heading{
+        text-transform: uppercase;
+        font-size: 6.4rem;
+    }
+
+    .hero-para {
+        margin-top: 1.5rem;
+        margin-bottom: 3.4rem;
+        max-width: 60rem;
+    }
+
+    .section-hero-image{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    picture {
+        text-align: center;
+    }
+
+    .hero-img{
+        max-width: 80%;
+    }
+
+`;
+export default HeroSection
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,34 +3,6 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const Navbar = () => {
-  const Nav = styled.nav`
-    .navbar-list {
-      display: flex;
-      gap: 4.8rem;
-
-      li {
-        list-style: none;
-
-        .navbar-link {
-          &:link,
-          &:visited {
-            display: inline-block;
-            text-decoration: none;
-            font-size: 1.8rem;
-            text-transform: uppercase;
-            color: ${({ theme }) => theme.colors.black};
-            transition: color 0.3s linear;
-          }
-
-          &:hover,
-          &:active {
-            color: ${({ theme }) => theme.colors.helper};
-          }
-        }
-      }
-    }
-  `;
-
   return (
     <Nav>
       <div className="menuIcon">
@@ -60,5 +32,33 @@ const Navbar = () => {
     </Nav>
   );
 };
+
+const Nav = styled.nav`
+    .navbar-list {
+      display: flex;
+      gap: 4.8rem;
+
+      li {
+        list-style: none;
+
+        .navbar-link {
+          &:link,
+          &:visited {
+            display: inline-block;
+            text-decoration: none;
+            font-size: 1.8rem;
+            text-transform: uppercase;
+            color: ${({ theme }) => theme.colors.black};
+            transition: color 0.3s linear;
+          }
+
+          &:hover,
+          &:active {
+            color: ${({ theme }) => theme.colors.helper};
+          }
+        }
+      }
+    }
+  `;
 
 export default Navbar;
