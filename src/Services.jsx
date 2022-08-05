60fd00724e91b40f5948b0a6dd02410b08bfe856
vivekmodi1301 <modivivek03@gmail.com>
2022-08-05 15:24:41 +0530

    Services

--- src/Services.jsx
+++ src/Services.jsx
@@ -1,7 +1,99 @@
 import React from "react";
-
+import styled from "styled-components";
+import { data } from "./data";
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <Wrapper className="section">
+    <h2 className="common-heading">Our Services</h2>
+    <div className="container grid grid-three-column">
+    {data.map((data, index) =>
+      <div key={index}>
+        <div className="card">
+          <figure>
+            <img className="card-img-top" src={data.image} alt="Card image" />
+          </figure>
+          <div className="card-data">
+            <h3 className="card-title">{data.title}</h3>
+            <p >{data.description}</p>
+            <a href="#" className="btn">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      )} 
+      </div>
+    </Wrapper>
+  )
 };
 
+const Wrapper = styled.section`
+  padding: 9rem 0;
+  /* background-color: ${({ theme }) => theme.colors.bg}; */
+
+  .container {
+    max-width: 120rem;
+  }
+  .card {
+    border: 0.1rem solid rgb(170 170 170 / 40%);
+    .card-data {
+      padding: 0 2rem;
+    }
+  h3 {
+      margin: 2rem 0;
+      font-weight: 300;
+      font-size: 2.4rem;
+    }
+  .btn {
+      margin: 2rem auto;
+      background-color: rgb(0 0 0 / 0%);
+      border: 0.1rem solid rgb(98 84 243);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: rgb(98 84 243);
+      font-size: 1.4rem;
+      &:hover {
+        background-color: rgb(98 84 243);
+        color: #fff;
+      }
+    }
+  }
+    figure {
+    width: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    overflow: hidden;
+    transition: all 0.5s linear;
+    &::after {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 0%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.5);
+      transition: all 0.2s linear;
+      cursor: pointer;
+    }
+    &:hover::after {
+      width: 100%;
+    }
+    &:hover img {
+      transform: scale(1.2);
+    }
+    img {
+      max-width: 90%;
+      margin-top: 1.5rem;
+      height: 20rem;
+      transition: all 0.2s linear;
+    }
+  }
+
+  @media (max-width: 790px) {
+    .grid-three-column {
+      grid-template-columns: 1fr;
+    }
+  }
+`
 export default Services;
